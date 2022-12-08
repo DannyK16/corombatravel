22a6c89839c55325afaf96d9517c67838fbd706f
Daniel Kutiriev <daniel.kutiriev@ugent.be>
2022-12-08 17:27:18 +0100

    tawk.to

--- components/technicalComponents/HeadComponent/HeadComponent.js
+++ components/technicalComponents/HeadComponent/HeadComponent.js
@@ -67,7 +67,7 @@ export default class HeadComponent extends Component {
 					(function(){
 					var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 					s1.async=true;
-					s1.src='https://tawk.to/chat/63920a64daff0e1306db9d29/1gjp7h6rt';
+					s1.src='https://embed.tawk.to/63920a64daff0e1306db9d29/1gjp7h6rt';
 					s1.charset='UTF-8';
 					s1.setAttribute('crossOrigin','*');
 					s0.parentNode.insertBefore(s1,s0);})();`,
